2c39f6ddc4d4ae7d2ec3130b95f2952af1c942ed
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-11-22 08:27:02 +0200

    fixed origin for production deploy

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ const config = {
   production: {
     port: process.env.PORT || 3000,
     dbURL: process.env.DB_URL_CREDENTIALS,
-    origin: [],
+    origin: ["https://ubuy-react.netlify.app"],
   },
 };
 
