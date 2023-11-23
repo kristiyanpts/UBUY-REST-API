a7cd98d66fe540fc62e9fd0c878807ed3fa73eb1
KrisDevBG <kristiqn3456@gmail.com>
2023-11-23 15:48:48 +0200

    added user model validation

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
     port: process.env.PORT || 3000,
     dbURL:
       "mongodb+srv://kristiqn3456:5JUmCOBlcKTc5eJs@ubuy-softuni-react.tvde8lf.mongodb.net/?retryWrites=true&w=majority",
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
   },
   production: {
     port: process.env.PORT || 3000,
--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = Number(process.env.SALTROUNDS) || 5;
 
+const URL_PATTERN = /^https?:\/\/.+$/i;
+const EMAIL_PATTERN = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+const LATIN_LETTERS_PATTERN = /[a-zA-Z0-9]+/g;
+
 const { ObjectId } = mongoose.Schema.Types;
 
 const userSchema = new mongoose.Schema(
@@ -9,15 +13,23 @@ const userSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
+      minlength: [2, "First Name should be at least 2 characters long"],
     },
     lastName: {
       type: String,
       required: true,
+      minlength: [2, "Last Name should be at least 2 characters long"],
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      validate: {
+        validator: function (v) {
+          return EMAIL_PATTERN.test(v);
+        },
+        message: (props) => `Email format is invalid!`,
+      },
     },
     username: {
       type: String,
@@ -26,7 +38,7 @@ const userSchema = new mongoose.Schema(
       minlength: [5, "Username should be at least 5 characters"],
       validate: {
         validator: function (v) {
-          return /[a-zA-Z0-9]+/g.test(v);
+          return LATIN_LETTERS_PATTERN.test(v);
         },
         message: (props) =>
           `${props.value} must contains only latin letters and digits!`,
@@ -38,7 +50,7 @@ const userSchema = new mongoose.Schema(
       minlength: [5, "Password should be at least 5 characters"],
       validate: {
         validator: function (v) {
-          return /[a-zA-Z0-9]+/g.test(v);
+          return LATIN_LETTERS_PATTERN.test(v);
         },
         message: (props) =>
           `${props.value} must contains only latin letters and digits!`,
@@ -47,10 +59,19 @@ const userSchema = new mongoose.Schema(
     pfpUrl: {
       type: String,
       required: true,
+      validate: {
+        validator: function (value) {
+          return URL_PATTERN.test(value);
+        },
+        message: "Image URL is invalid",
+      },
     },
     role: {
       type: String,
-      enum: ["buyer", "seller", "both"],
+      enum: {
+        values: ["buyer", "seller", "both"],
+        message: "Role is invalid!",
+      },
       required: true,
     },
     products: [
