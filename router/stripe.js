5d86bc30d641de80ba1e6a2d06e4a17013ad2b5a
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-12-14 17:18:57 +0200

    fixed some stripe things

--- router/stripe.js
+++ router/stripe.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const { auth } = require("../utils");
 
 const stripe = require("stripe")(
-  "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"
+  "sk_test_51OMtawBPikfn3zPt9fpDvnCd7eK5D90DPT4lZZW0klhm1bLGKnY3Rl9WOFzCIPF1SaGWNrCjW2z7YTGFsIBOZ9oc00qzFVtgTZ"
 );
 
 const calculateOrderAmount = (items) => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return 1400;
+  let totalPrice = 0;
+
+  items.forEach((i) => {
+    totalPrice += i.price * (i.quantityBuying || 1);
+  });
+
+  return totalPrice * 100;
 };
 
 router.post("/create-payment-intent", auth(), async (req, res) => {
@@ -19,7 +22,7 @@ router.post("/create-payment-intent", auth(), async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "bgn",
+    currency: "usd",
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
